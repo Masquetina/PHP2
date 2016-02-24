c93f5bf37697b2463652c7fd992e0702839dccdc
Masquetina <iva.dopudj@gmail.com>
2016-02-24 18:50:10 +0100

    Add banned check

--- application/models/Validate.php
+++ application/models/Validate.php
@@ -5,20 +5,31 @@ class Validate extends CI_Model {
   public function login() {
     $this->db->where('email', $this->input->post('email'));
     $this->db->where('password', $this->input->post('password'));
+
     $query = $this->db->get('users');
+
     if($query->num_rows() == 1) {
       $row = $query->row();
-			$data = array(
-        'id_user'   => $row->id_user,
-				'username'  => $row->username,
-				'email'     => $row->email,
-				'id_rolle'  => $row->id_rolle,
-        'avatar'    => $row->avatar,
-        'validated' => TRUE
-			);
-      $message = 'Hi ' . ucwords($data['username']) . '! Wellcome.';
-			$this->session->set_userdata($data);
-      $this->session->set_flashdata('message', $message);
+
+      $ban = $row->ban;
+
+      if($ban == 0) {
+        $data = array(
+          'id_user'   => $row->id_user,
+  				'username'  => $row->username,
+  				'email'     => $row->email,
+  				'id_rolle'  => $row->id_rolle,
+          'avatar'    => $row->avatar,
+          'validated' => TRUE
+  			);
+        $message = 'Hi ' . ucwords($data['username']) . '! Wellcome.';
+  			$this->session->set_userdata($data);
+        $this->session->set_flashdata('message', $message);
+      } else {
+        $message = 'Ups! Are You banned? Logging is disabled.';
+        $this->session->set_flashdata('message', $message);
+      }
+      return TRUE;
 		}
   }
 
