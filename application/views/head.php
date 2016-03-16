1a535f28f60d17b783db08b06ef9e2d6bf3868f0
Masquetina <iva.dopudj@gmail.com>
2016-03-16 08:07:18 +0100

    Favicon

--- application/views/head.php
+++ application/views/head.php
@@ -7,6 +7,8 @@
 	<meta name="keywords" content="Quote, Quotations">
 	<meta name="author" content="Iva Dopuđ">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="<?=base_url();?>custom/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300italic,300,700&subset=latin,latin-ext" type="text/css">
 	<link rel="stylesheet" href="https://cdn.materialdesignicons.com/1.4.57/css/materialdesignicons.min.css" type="text/css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" type="text/css">
